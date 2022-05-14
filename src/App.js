6ece6f4ec60d10d5b10a44aa378abe816a802e53
MichaelCostanzaJr <77464599+MichaelCostanzaJr@users.noreply.github.com>
2022-05-14 11:41:54 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <h1>Welcome to my super store!!!</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h1>This is the footer</h1>
+    </div>
+  );
+}
+
+export default Footer;
